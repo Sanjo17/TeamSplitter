d179e74db2d1f1668821066cf1c751e64b719966
Sanjo <sanjoneyyan@gmail.com>
2024-10-18 18:36:22 +0530

    style change

--- src/components/PlayerTile.jsx
+++ src/components/PlayerTile.jsx
@@ -5,9 +5,9 @@ function PlayerTile({player = "Player",remove,id}){
     return(
         <div className='playertile'>
             <div className='tile-items'>
-        <h4>{
+        <h4 >{
         player!=""?player.toUpperCase():"player"}</h4>
-        <Button title='x' padding='6px ' onSumit={()=>remove(id)} bg='orange'/>
+        <Button title='x' padding='6px ' onSumit={()=>remove(id)} bg='#C1B9AC'/>
         </div>
         </div>
     );
